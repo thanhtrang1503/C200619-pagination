fda9296ba17a187a4982cc8371078a0a9f8dac5a
ccc-goto <42729329+ccc-goto@users.noreply.github.com>
2021-10-27 14:04:04 +0900

    up

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 ﻿<?php
+require_once("config.php");
 function db_conn() {
     /* データベースへ接続　*/
     try {
-        $dbh = new PDO(DNS,DB_USER,DB_PASS);
+        $dbh = new PDO(DSN,DB_USER,DB_PASS);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     } catch (PDOException $e){
