35f0af3758692e04f352418334e6e833843e8156
ccc-goto <42729329+ccc-goto@users.noreply.github.com>
2021-10-27 11:02:26 +0900

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,9 +1,5 @@
 ﻿<?php
 function db_conn() {
-    /* データベース定義 */
-    define('DNS','mysql:host=localhost;dbname=lesson1;charset=utf8mb4');
-    define('DB_USER','root');
-    define('DB_PASS','');
     /* データベースへ接続　*/
     try {
         $dbh = new PDO(DNS,DB_USER,DB_PASS);
